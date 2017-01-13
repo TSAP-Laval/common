bd83673924953ea91e27d638283f1b89dfed5122
Ressius <tonydumu@hotmail.com>
2017-01-13 10:12:23 -0500

    Modification des sizes + commentaires

--- common/models.go
+++ common/models.go
@@ -9,9 +9,9 @@ import (
 // et ses équipes
 type Entraineur struct {
 	gorm.Model
-	Prenom   string `gorm:"size:45"`
-	Nom      string `gorm:"size:45"`
-	Email    string `gorm:"size:45"`
+	Prenom   string
+	Nom      string
+	Email    string
 	PassHash string
 	Token    string
 	Equipes  []Equipe `gorm:"many2many:entraineur_equipe;"`
@@ -41,44 +41,44 @@ type Saison struct {
 // pour un match
 type Lieu struct {
 	gorm.Model
-	Nom     string `gorm:"size:45"`
-	Ville   string `gorm:"size:45"`
-	Adresse string `gorm:"size:45"`
+	Nom     string
+	Ville   string
+	Adresse string
 }
 
 // TypeAction est une modélisation des types
 // possibles  d'action
 type TypeAction struct {
 	gorm.Model
-	Nom string `gorm:"size:45"`
+	Nom string
 }
 
 // Sport est une modélisation des noms possibles
 // des sports pratiqués
 type Sport struct {
 	gorm.Model
-	Nom string `gorm:"size:45"`
+	Nom string
 }
 
 // Zone est une modélisation de la zone de jeu
 // de l'action
 type Zone struct {
 	gorm.Model
-	Nom string `gorm:"size:45"`
+	Nom string
 }
 
 // Niveau est une modélisation du niveau d'un joueur
 type Niveau struct {
 	gorm.Model
-	Nom string `gorm:"size:45"`
+	Nom string
 }
 
 // Equipe est une modélisation d'une équipe de Joueurs
 // dirigiée par un ou plusieurs entraineurs
 type Equipe struct {
 	gorm.Model
-	Nom         string `gorm:"size:45"`
-	Ville       string `gorm:"size:45"`
+	Nom         string
+	Ville       string
 	Sport       Sport
 	SportID     int
 	Niveau      Niveau
@@ -121,19 +121,25 @@ type Action struct {
 	JoueurID        int
 }
 
+// Administrateur est une modélisation des informations
+// de connexion d'un Administrateur
 type Administrateur struct {
 	gorm.Model
 	Email                 string
 	PassHash              string
 	TokenReinitialisation string
-	TokenConexion         string
+	TokenConnexion        string
 }
 
+// Position est une modélisation du nom de la Position
+// du joueur
 type Position struct {
 	gorm.Model
-	Nom string `gorm:"size:45"`
+	Nom string
 }
 
+// JoueurPositionPartie est une modélisation de la Position
+// d'un joueur dans une partie
 type JoueurPositionPartie struct {
 	gorm.Model
 	Joueur     Joueur
@@ -144,6 +150,7 @@ type JoueurPositionPartie struct {
 	PositionID int
 }
 
+// Metrique est la modélisation d'une unité de calcul
 type Metrique struct {
 	gorm.Model
 	Nom      string `gorm:"primary_key"`
@@ -152,6 +159,8 @@ type Metrique struct {
 	EquipeID int
 }
 
+// Video est la modélisation des informations
+// sur une Video
 type Video struct {
 	gorm.Model
 	Path           string
