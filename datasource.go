b1e7eae95b759a945996b3133292e3a5d78e6f65
William Dussault <dalloriam@gmail.com>
2017-02-19 14:55:28 -0500

    Add player positions (#19)

--- datasource.go
+++ datasource.go
@@ -114,6 +114,37 @@ func (d *Datasource) GetMatches(playerID uint, teamID uint, seasonID uint) ([]mo
 	return matches, err
 }
 
+// GetPositions retourne une liste de positions occupées par le joueur
+func (d *Datasource) GetPositions(playerID int) ([]models.Position, error) {
+	var err error
+
+	db, err := gorm.Open(d.dbType, d.dbConn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer db.Close()
+
+	positionsMap := make(map[models.Position]bool)
+	positions := []models.Position{}
+
+	positionParties := []models.JoueurPositionPartie{}
+
+	db.Where(models.JoueurPositionPartie{JoueurID: playerID}).Find(&positionParties)
+
+	for i := 0; i < len(positionParties); i++ {
+		positionParties[i].Expand(db)
+		positionsMap[positionParties[i].Position] = true
+	}
+
+	for k := range positionsMap {
+		positions = append(positions, k)
+	}
+
+	return positions, nil
+}
+
 // GetMatch obtient toutes les informations sur un match
 func (d *Datasource) GetMatch(matchID uint) (*models.Partie, error) {
 	var err error
