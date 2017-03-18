1b09f030d690216fdbdccbfb22d7e71290f37a2c
William Dussault <dalloriam@gmail.com>
2017-03-18 14:12:08 -0400

    Fix interface TODOs

--- datasource.go
+++ datasource.go
@@ -15,9 +15,9 @@ type IDatasource interface {
 	GetSeasons() (*[]models.Saison, error)
 	GetTeam(teamID uint) (*models.Equipe, error)
 	GetPlayer(playerID uint) (*models.Joueur, error)
-	GetMatches(teamID uint, seasonID uint) ([]models.Partie, error)       // TODO: Why no pointer here?
-	GetMatchPosition(playerID int, matchID int) (*models.Position, error) // TODO: Why no int here?
-	GetPositions(playerID int) ([]models.Position, error)                 // TODO: Why no pointer here? Why no uint?
+	GetMatches(teamID uint, seasonID uint) (*[]models.Partie, error)
+	GetMatchPosition(playerID uint, matchID uint) (*models.Position, error)
+	GetPositions(playerID uint) (*[]models.Position, error)
 	GetMatch(matchID uint) (*models.Partie, error)
 	GetLatestMatch(teamID uint) (*models.Partie, error)
 	GetCoach(coachID uint) (*models.Entraineur, error)
@@ -119,7 +119,7 @@ func (d *Datasource) GetPlayer(playerID uint) (*models.Joueur, error) {
 }
 
 // GetMatches obtient les match d'un joueur (pour une équipe, pour une saison, pour une position?)
-func (d *Datasource) GetMatches(teamID uint, seasonID uint) ([]models.Partie, error) {
+func (d *Datasource) GetMatches(teamID uint, seasonID uint) (*[]models.Partie, error) {
 	var err error
 
 	db, err := gorm.Open(d.dbType, d.dbConn)
@@ -144,11 +144,11 @@ func (d *Datasource) GetMatches(teamID uint, seasonID uint) ([]models.Partie, er
 		}
 	}
 
-	return matches, err
+	return &matches, err
 }
 
 // GetMatchPosition retourne la position occupée par un joueur pour un match
-func (d *Datasource) GetMatchPosition(playerID int, matchID int) (*models.Position, error) {
+func (d *Datasource) GetMatchPosition(playerID uint, matchID uint) (*models.Position, error) {
 	var err error
 
 	db, err := gorm.Open(d.dbType, d.dbConn)
@@ -160,9 +160,9 @@ func (d *Datasource) GetMatchPosition(playerID int, matchID int) (*models.Positi
 
 	posPartie := models.JoueurPositionPartie{}
 
-	db.Where(models.JoueurPositionPartie{JoueurID: playerID, PartieID: matchID}).First(&posPartie)
+	db.Where(models.JoueurPositionPartie{JoueurID: int(playerID), PartieID: int(matchID)}).First(&posPartie)
 
-	if posPartie.JoueurID != playerID {
+	if posPartie.JoueurID != int(playerID) {
 		return nil, fmt.Errorf("No position for player %d", playerID)
 	}
 
@@ -174,7 +174,7 @@ func (d *Datasource) GetMatchPosition(playerID int, matchID int) (*models.Positi
 }
 
 // GetPositions retourne une liste de positions occupées par le joueur
-func (d *Datasource) GetPositions(playerID int) ([]models.Position, error) {
+func (d *Datasource) GetPositions(playerID uint) (*[]models.Position, error) {
 	var err error
 
 	db, err := gorm.Open(d.dbType, d.dbConn)
@@ -190,7 +190,7 @@ func (d *Datasource) GetPositions(playerID int) ([]models.Position, error) {
 
 	positionParties := []models.JoueurPositionPartie{}
 
-	db.Where(models.JoueurPositionPartie{JoueurID: playerID}).Find(&positionParties)
+	db.Where(models.JoueurPositionPartie{JoueurID: int(playerID)}).Find(&positionParties)
 
 	for i := 0; i < len(positionParties); i++ {
 		positionParties[i].Expand(db)
@@ -201,7 +201,7 @@ func (d *Datasource) GetPositions(playerID int) ([]models.Position, error) {
 		positions = append(positions, k)
 	}
 
-	return positions, nil
+	return &positions, nil
 }
 
 // GetMatch obtient toutes les informations sur un match
